ee626bc67c934dbb85e00841eba5ccf1196db31a
Gravity <gravityworkflowpro@gmail.com>
2025-03-19 09:18:15 -0400

    Debug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: env.VITE_BASE_URL,
+    // base: env.VITE_BASE_URL,
+    base: '/gravityworkflow.github.io',
     server: {
       port: 3000,
       open: true,
